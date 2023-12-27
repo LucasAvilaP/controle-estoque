b274285df49bb85ffe3f32058c33c5ae82dde304
Lucas Avila <Lukas.avila02@gmail.com>
2023-12-27 15:30:09 -0300

    adicionando funcionalidade de exportação para excel

--- controle_estoque/gestao_estoque/urls.py
+++ controle_estoque/gestao_estoque/urls.py
@@ -3,13 +3,14 @@
 from django.urls import path
 from . import views
 from django.urls import path
-from .views import buscar_produtos, atualizar_quantidade, diminuir_quantidade
+from .views import buscar_produtos, atualizar_quantidade, diminuir_quantidade, exportar_produtos_xlsx
 
 urlpatterns = [
     path('', views.pagina_inicial, name='pagina_inicial'),
     path('buscar-produtos/', views.buscar_produtos, name='buscar_produtos'),
     path('atualizar_quantidade/', views.atualizar_quantidade, name='atualizar_quantidade'),
     path('diminuir_quantidade/', views.diminuir_quantidade, name='diminuir_quantidade'),
+    path('exportar_produtos_xlsx/', views.exportar_produtos_xlsx, name='exportar_produtos_xlsx'),
 
 ]
 
--- controle_estoque/gestao_estoque/views.py
+++ controle_estoque/gestao_estoque/views.py
@@ -1,8 +1,10 @@
 # gestao_estoque/views.py
 
+import csv
+import xlsxwriter
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from produtos.models import Produto  # Ajuste o caminho do import conforme necessário
 from django.views.decorators.csrf import csrf_exempt
 import json
@@ -55,3 +57,27 @@ def diminuir_quantidade(request):
             return JsonResponse({'success': False, 'error': str(e)}, status=500)
 
     return JsonResponse({'success': False, 'error': 'Método não permitido.'}, status=405)
+
+
+def exportar_produtos_xlsx(request):
+    # Cria uma resposta do tipo HttpResponse com o tipo de conteúdo correto
+    response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    response['Content-Disposition'] = 'attachment; filename="relatorio_produtos.xlsx"'
+
+    # Cria um arquivo Excel na memória
+    workbook = xlsxwriter.Workbook(response, {'in_memory': True})
+    worksheet = workbook.add_worksheet()
+
+    # Escreve o cabeçalho
+    worksheet.write('A1', 'Nome do Produto')
+    worksheet.write('B1', 'Quantidade')
+
+    # Escreve os dados dos produtos
+    for idx, produto in enumerate(Produto.objects.all().values_list('nome', 'quantidade'), start=1):
+        worksheet.write_string(idx, 0, produto[0]) # Nome do produto
+        worksheet.write_number(idx, 1, produto[1]) # Quantidade
+
+    # Fecha o arquivo Excel
+    workbook.close()
+
+    return response
